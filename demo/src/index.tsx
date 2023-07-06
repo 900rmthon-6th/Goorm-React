d24f748a7e3d3d18e79c64fcd1d3c647a730b5f1
Gunho Park <devgun@kakao.com>
2023-07-07 02:10:49 +0900

    feat: curl mbti all

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,19 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Box, Paper, Typography } from '@mui/material';
+
+const PrettyResponse = () => {
+	const [response, setResponse] = useState("");
+
+	useEffect(() => {
+		// Fetch the data
+		fetch("http://time2do.works/mbti/all")
+			.then((res) => res.json())
+			.then((data) => {
+				// Format the response with indentation
+				const formattedResponse = JSON.stringify(data, null, 2);
+				setResponse(formattedResponse);
+			})
+			.catch((error) => {
+				console.error("Error fetching data:", error);
+			});
+	}, []);
+
+	return (
+    <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+      <Paper elevation={3} sx={{ padding: '2rem' }}>
+        <Typography variant="h6" component="pre">
+          {response}
+        </Typography>
+      </Paper>
+    </Box>
+  );
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<PrettyResponse />
+		<App />
+	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
